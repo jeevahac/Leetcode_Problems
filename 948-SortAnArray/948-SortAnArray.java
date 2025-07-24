0b0ef8194b63e7b27c418c7e29fbc3c81093a78e
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-07-24 21:24:16 +0530

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.java
@@ -0,0 +1,9 @@
+// Last updated: 7/24/2025, 9:24:15 PM
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);  // Using inbuilt Timsort
+        return nums;        // Return the sorted array
+    }
+}
