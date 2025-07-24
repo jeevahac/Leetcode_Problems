f51ce785f41662b9e25c15e147fa58cf8113341e
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-07-24 21:25:30 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 7/24/2025, 9:25:28 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
