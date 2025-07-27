60679920cebb8f39d87a155610157059d9849cc9
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-07-27 16:03:27 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/version/java/48-RotateImage_20250727_160328.java
@@ -0,0 +1,27 @@
+// Last updated: 7/27/2025, 4:03:28 PM
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int n=matrix.length;
+
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
