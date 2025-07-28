321a14653bf98355cce452d2c219839969dec9f3
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-07-28 10:35:58 +0530

    Create 33-SearchinRotatedSortedArray

---
+++ 33-SearchinRotatedSortedArray/version/java/33-SearchinRotatedSortedArray_20250728_103558.java
@@ -0,0 +1,47 @@
+// Last updated: 7/28/2025, 10:35:58 AM
+class Solution {
+
+    public static int binarysearch(int nums[],int target){
+        
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<=right){
+            int mid=(left+right)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            // to check whether the left side array is sorted
+            else if(nums[left]<=nums[mid]){
+
+                if(nums[left]<=target && target<nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+                
+            }
+
+            else{
+                if(nums[mid]<target && target<=nums[right]){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+                
+            }
+        }
+
+        return -1;
+    }
+    public int search(int[] nums, int target) {
+        
+        int index=binarysearch(nums,target);
+
+        return index;
+    }
+}
