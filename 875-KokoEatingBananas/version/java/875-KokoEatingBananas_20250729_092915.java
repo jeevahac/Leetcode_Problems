926f41b765514ca21ad4f89414d6b7a8c9468d38
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-07-29 09:29:15 +0530

    Create 875-KokoEatingBananas

---
+++ 875-KokoEatingBananas/version/java/875-KokoEatingBananas_20250729_092915.java
@@ -0,0 +1,43 @@
+// Last updated: 7/29/2025, 9:29:15 AM
+class Solution {
+
+    public static int time_taken(int arr[], int k){
+
+        int total_hour=0;
+
+        for(int i=0;i<arr.length;i++){
+            total_hour+= Math.ceil((double) arr[i]/k);
+        }
+
+        return total_hour;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        int max=Integer.MIN_VALUE;
+        for(int num: piles){
+            if(num>max){
+                max=num;
+            }
+        }
+
+        int low=1;
+        int high=max;
+        int ans=1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            int hours=time_taken(piles,mid);
+
+            if(hours<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
