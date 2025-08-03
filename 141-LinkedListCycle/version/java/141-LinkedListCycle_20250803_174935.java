dc7304d133e465c2090b03e6204115cdd70aed91
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-08-03 17:49:35 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/version/java/141-LinkedListCycle_20250803_174935.java
@@ -0,0 +1,28 @@
+// Last updated: 8/3/2025, 5:49:35 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode ptrA = headA;
+        ListNode ptrB = headB;
+
+        // After at most 2 traversals each, they will either meet or both be null
+        while (ptrA != ptrB) {
+            ptrA = (ptrA == null) ? headB : ptrA.next;
+            ptrB = (ptrB == null) ? headA : ptrB.next;
+        }
+
+        return ptrA; // can be intersection node or null
+    }
+}
