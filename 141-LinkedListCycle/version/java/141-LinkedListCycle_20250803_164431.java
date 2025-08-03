ce938770b479a4304bd05b08fd224afc4419263a
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-08-03 16:44:31 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/version/java/141-LinkedListCycle_20250803_164431.java
@@ -0,0 +1,29 @@
+// Last updated: 8/3/2025, 4:44:31 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev=null;
+        ListNode current=head;
+        ListNode next=null;
+
+        while(current!=null){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+
+        return prev;
+        
+    }
+}
