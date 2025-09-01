ea9b89cded24c5d85fbe5efc1af7fd5f9948bf3d
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-09-01 08:23:14 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/version/java/416-PartitionEqualSubsetSum_20250901_082313.java
@@ -0,0 +1,22 @@
+// Last updated: 9/1/2025, 8:23:13 AM
+class Solution {
+    public boolean canPartition(int[] arr) {
+        
+        int sum = 0;
+        for (int num : arr) sum += num;
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : arr) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+                if (dp[target]) return true;
+            }
+        }
+
+        return dp[target];
+    }
+}
