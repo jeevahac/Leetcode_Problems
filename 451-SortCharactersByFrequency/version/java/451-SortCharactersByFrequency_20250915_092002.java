d3f82d21da5e5a99fd98efe9c0ae52c913aab931
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-09-15 09:20:05 +0530

    Create 451-SortCharactersByFrequency

---
+++ 451-SortCharactersByFrequency/version/java/451-SortCharactersByFrequency_20250915_092002.java
@@ -0,0 +1,32 @@
+// Last updated: 9/15/2025, 9:20:02 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        ListNode temp=head;
+
+        if(temp==null){
+            return head;
+        }
+
+        while(temp.next!=null){
+            if(temp.val==temp.next.val){
+                temp.next=temp.next.next;
+            }
+            else{
+                temp=temp.next;
+            }
+        }
+
+        return head;
+    }
+}
