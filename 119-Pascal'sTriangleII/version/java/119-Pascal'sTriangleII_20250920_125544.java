c724937274b7652db21975bd7bfd20bbbd18b33e
jeevahac <94861329+jeevahac@users.noreply.github.com>
2025-09-20 12:55:45 +0530

    Create 119-Pascal'sTriangleII

---
+++ 119-Pascal'sTriangleII/version/java/119-Pascal'sTriangleII_20250920_125544.java
@@ -0,0 +1,20 @@
+// Last updated: 9/20/2025, 12:55:44 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        Map<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+
+            if(entry.getValue()==1){
+                return entry.getKey();
+            }
+        }
+
+        return -1;
+    }
+}
